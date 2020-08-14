5a8cadfdee25c1e2737be62c5a10cfe54f957e4b
JahedHossenBangladesh <jahedhossen4358.com>
2020-08-14 23:42:17 +0600

    this is template  String

---
+++ templateString.js
@@ -0,0 +1,10 @@
+var a = 12;
+var b = 22;
+var strng = `this is the 
+string`;
+
+console.log(strng + ( a  +b));
+
+console.log( `this is the string of
+the templateString.
+and also the sum of a and b  ${ a+ b } and also the double ${ 2 * a + b}`);
